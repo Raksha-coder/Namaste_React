da5382f5ca592a1d4fccec00cfd589240d9e78c1
raksha-coder <ku.rakshadilipjaiswal@gmail.com>
2025-09-09 21:07:52 +0530

    Lecture 3

--- App.js
+++ App.js
@@ -2,26 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-// this is very confusing structure that's why we use JSX
-const div = React.createElement(
-  "div",
-  {id:"parent"},
-  React.createElement(
-    'div',
-    {id:"child"},
-    [React.createElement('h1',{},"Hi There"),React.createElement('h1',{},"Hi There")]
-  ));
+const jsxHeading = <h1>This is JSX</h1>;
 
-const heading = React.createElement(
-  "h1", {id:"heading"}, "Hello world from react"
-);
-
-console.log(heading); //object
-
-//we have created our react element , now we need to render it, so we will use ReactDOM for that.
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(div);
 
 
 
